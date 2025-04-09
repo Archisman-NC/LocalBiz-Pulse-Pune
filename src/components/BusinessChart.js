d204df67cb39fc6b97b0b27afbc1c2613945958c
Archisman <archismannc2005@gmail.com>
2025-04-09 18:08:13 +0530

    disable chart

--- src/components/BusinessChart.js
+++ src/components/BusinessChart.js
@@ -26,7 +26,7 @@ const BusinessChart = ({ data }) => {
   return (
     <div className="chart-container" style={{ padding: '1rem' }}>
       <h2 style={{ textAlign: 'center' }}>Top 8 Business Types in Pune</h2>
-      <ResponsiveContainer width="100%" height={350}>
+      {/* <ResponsiveContainer width="100%" height={350}>
         <BarChart
           data={chartData}
           margin={{ top: 20, right: 30, left: 20, bottom: 60 }}
@@ -38,7 +38,7 @@ const BusinessChart = ({ data }) => {
           <Legend />
           <Bar dataKey="count" fill="#8884d8" barSize={40} radius={[10, 10, 0, 0]} />
         </BarChart>
-      </ResponsiveContainer>
+      </ResponsiveContainer> */}
     </div>
   );
 };
